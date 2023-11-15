8688f012753c0f81343a31e53e06fd0ddee63143
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-15 18:37:12 +0500

    today

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+import Link from "next/link"
+import Image from "next/image"
+
 
 // create function and export
 
 let Home = () => {
      return(
-        <>
+        <div>
         
         <h1> heading </h1>
 
         
-        </>
+        </div>
         
      )
 }
