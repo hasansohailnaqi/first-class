45acfc97d40728ffd861ccc2ad00136257f9d854
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-15 19:49:14 +0500

    class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+'use client' // client site rendering
+
 import Link from "next/link"
 import Image from "next/image"
+
+
+const Clicked = () => {
+   alert ("Clicked")
+}
 
 
 // create function and export
 
 let Home = () => {
-     return(
+     return( 
         <div>
         
         <h1> heading </h1>
-        <Link href="about">hasan</Link>  
+        <Link href="about">hasan</Link>
+        
+        <button onClick={Clicked}>click me</button>
         
         </div>
         
