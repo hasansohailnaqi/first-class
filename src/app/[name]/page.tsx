7c3ad665a041a13cdd3d58ec8910c522ba3ee64f
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-15 20:55:38 +0500

    class

---
+++ src/app/[name]/page.tsx
@@ -0,0 +1,15 @@
+const About = (props:any) => {
+    console.log()
+    return (
+  
+        <><h2>{props.params.name} hasan profile </h2>
+        </>
+    ) 
+
+}
+
+export default About
+
+const ob ={
+    params: {name: `hasan`}
+}
